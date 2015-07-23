032db1b594e786bf0cfc327153141c85a31d9a14
Micah Murray <micah.murray@outlook.com>
2015-07-23 22:34:49 +0000

    making it a working app and stuff

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ var remoteXHR = 'http://micahj.com/code/displayit/request.php';
     MACaddress = false,
     displayit_id = false;
 
+// reload every minute
+setTimeout("window.location.reload();",60000);
+
+// set up "device ready"
 document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 
@@ -12,28 +16,44 @@ function onDeviceReady() {
     {
     	window.MacAddress.getMacAddress(
     		function(macAddress) {
-    			$("#pagebody").html("Fetching Account infor");
+    			$("#status").html("Fetching Account info").fadeIn(0);
     			$("#mac").html("MAC Address: "+macAddress);
-                $.getJSON(remoteXHR,{mac:macAddress},function(){
+                $.getJSON(remoteXHR,{action:'checkDevice',mac:macAddress},function(data){
                     if(!data)
                     {
                         $("#pagebody").load('views/register.html');
+                        $("#status").fadeOut('fast');
                     }
                     else
                     {
-                        alert("great, we know who you are.  here's some contetn");
+                        displayit_id = data;
+                        $("#mac").fadeOut('fast');
+                        loadContent();
                     }
-                })
+                });
     		},	
     		function(failed) {
     			alert(failed);
     		}
     	);        
     }
-
-
-	
+    else
+    {
+        loadContent();
+    }
 	// turn on "auto start" for this device on load.
 	cordova.plugins.autoStart.enable();
+} // end "on Device Ready"
 
+function loadContent()
+{
+    $("#status").html('checking...').fadeIn(0);
+    $.get(remoteXHR,{action:'getContent',id:displayit_id},function(data){
+        var current_display = $("#pagebody").html();
+        if(data != current_display)
+        {
+            $("#pagebody").html(data);            
+        }
+        $("#status").fadeOut('fast');
+    });
 }
